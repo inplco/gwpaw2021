a94cb0396d39aa48a6c1d6a5f327d8cb94bc8f3c
inplco <inplco@protonmail.com>
2022-02-21 14:47:02 +0100

    project closed

--- voting-poster/src/components/Landing/Landing.js
+++ voting-poster/src/components/Landing/Landing.js
@@ -59,10 +59,13 @@ export default function Landing() {
         <div className="app-logo"></div>
         <div className="login-wrapper" >
           <h1 style={{ marginTop: -30, fontFamily: 'Rajdhani' }}>GWPAW 2021</h1>
-          <p style={{ marginTop: -60, marginBottom: 20 }}>DEC 14-17 2021, HANNOVER</p>
+          <p style={{ marginTop: -50, marginBottom: 20 }}>DEC 14-17 2021, HANNOVER</p>
           <h2 style={{ marginTop: 10, fontFamily: 'Rajdhani' }}>VOTING PAGE</h2>
-          <p style={{ marginTop: -40, color:'orange', fontSize:18 }}>VOTING ENDED AT 23 DEC 2021 17:00 CET</p>
-          {/*<Countdown title="VOTING ENDED AT 23 DEC 2021 17:00 CET" value={deadline} format="D:HH:m:s" style={{ marginTop: -40, color:'orange', fontWeight:600 }} />*/}
+          <p style={{ marginTop: -30, color:'orange', fontSize:16 }}>VOTING ENDED AT 23 DEC 2021 17:00 CET</p>
+          <p style={{ fontWeight: 800, color:'white', fontSize:28 }}>WINNERS</p>
+          <p style={{ marginTop: 0, color:'white', fontSize:22 }}><a href="https://solscan.io/token/EkFg7wou6shRx2mpGdKHic9EDVxmbWnv7NnxZunEfEyj" target="_" style={{ fontWeight: 600, color:'skyblue', fontSize:22, textDecoration:'none' }}>JÁNOS TAKÁTSY</a></p>
+          <p style={{ marginTop: 0, color:'white', fontSize:22 }}><a href="https://solscan.io/token/AAy9CzPuUeVoZL5qadfKFpPrkf8SA1mX8PhyT3Dsaui7" target="_" style={{ fontWeight: 600, color:'skyblue', fontSize:22, textDecoration:'none' }}>YUMENG XU</a></p>
+          {/*<Countdown title="VOTING ENDED AT 23 DEC 2021 17:00 CET" value={deadline} format="D:HH:m:s" style={{ marginTop: -40, color:'orange', fontWeight:600 }} />
           <h5 style={{ fontSize:24, marginTop: 30 }}>USE REGISTRATION TOKEN TO ACCESS THE VOTING PAGE</h5>
           <button className="button-help" onClick={toggleModal}>Where is my token?</button>
           <Modal
@@ -84,8 +87,8 @@ export default function Landing() {
           <p style={{ fontSize:16, fontWeight:600, marginTop: -10, color: 'yellow' }}>HELP DESK: inplco@protonmail.com</p>
           <form onSubmit={authenticate} style={{ marginTop: -10 }}>
             <label>
-              {/*<input placeholder="registered email" type="text" onChange={e => setUsername(e.target.value)} style={{ fontFamily: 'Rajdhani', fontSize: 16 }} />
-              <br></br>*/}
+              <input placeholder="registered email" type="text" onChange={e => setUsername(e.target.value)} style={{ fontFamily: 'Rajdhani', fontSize: 16 }} />
+              <br></br>
               <input disabled="true" placeholder="token" type="password" onChange={e => setPassword(e.target.value)} style={{ fontFamily: 'Rajdhani', fontSize:16, marginTop:10 }} />
             </label>
             <div style={{ marginTop: 20, marginBottom: 20 }}>
@@ -93,6 +96,7 @@ export default function Landing() {
             </div>
             <br></br><br></br>
           </form>
+          */}
         </div>
         <div className="footer" style={{ position:'fixed' }}>
           <div>
